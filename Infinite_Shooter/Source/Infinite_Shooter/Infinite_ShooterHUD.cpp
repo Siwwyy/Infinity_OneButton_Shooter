15d395db22d6f167c7e41772da01aaca2b02d078
Damian Andrysiak <andrydamian@gmail.com>
2020-10-18 12:48:35 +0200

    changed CrossHairDrawPosition, Ten floats lower

--- Infinite_Shooter/Source/Infinite_Shooter/Infinite_ShooterHUD.cpp
+++ Infinite_Shooter/Source/Infinite_Shooter/Infinite_ShooterHUD.cpp
@@ -25,11 +25,11 @@ void AInfinite_ShooterHUD::DrawHUD()
 	const FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
 
 	// offset by half the texture's dimensions so that the center of the texture aligns with the center of the Canvas
-	const FVector2D CrosshairDrawPosition( (Center.X),
-										   (Center.Y));
+	const FVector2D CrosshairDrawPosition((Center.X),
+		(Center.Y + 10.f));
 
 	// draw the crosshair
-	FCanvasTileItem TileItem( CrosshairDrawPosition, CrosshairTex->Resource, FLinearColor::White);
+	FCanvasTileItem TileItem(CrosshairDrawPosition, CrosshairTex->Resource, FLinearColor::White);
 	TileItem.BlendMode = SE_BLEND_Translucent;
-	Canvas->DrawItem( TileItem );
+	Canvas->DrawItem(TileItem);
 }
