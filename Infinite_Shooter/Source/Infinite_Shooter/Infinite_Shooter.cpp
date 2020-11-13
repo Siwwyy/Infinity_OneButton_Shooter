a90df8cfd55d13dba03328b036f437dc23ba41f2
Damian Andrysiak <andrydamian@gmail.com>
2020-11-13 15:37:07 +0100

    small update and clean up

--- Infinite_Shooter/Source/Infinite_Shooter/Infinite_Shooter.cpp
+++ Infinite_Shooter/Source/Infinite_Shooter/Infinite_Shooter.cpp
@@ -1,6 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#include "Infinite_Shooter.h"
+#include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Infinite_Shooter, "Infinite_Shooter" );
